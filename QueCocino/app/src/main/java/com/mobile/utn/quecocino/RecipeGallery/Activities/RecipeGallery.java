fd1c666aa44d55d318cd9f0053c728cc78b2162e
Franco Milano Pesce <francomilanopesce@gmail.com>
2017-09-25 23:41:09 -0300

    Ademas de cargar fotos de la galeria se puede sacar foto con la camara

--- QueCocino/app/src/main/java/com/mobile/utn/quecocino/RecipeGallery/Activities/RecipeGallery.java
+++ QueCocino/app/src/main/java/com/mobile/utn/quecocino/RecipeGallery/Activities/RecipeGallery.java
@@ -1,9 +1,13 @@
 package com.mobile.utn.quecocino.RecipeGallery.Activities;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -26,8 +30,12 @@ import com.mobile.utn.quecocino.RecipeGallery.Adapters.GalleryAdapter;
 import com.mobile.utn.quecocino.RecipeGallery.Adapters.RecyclerAdapter;
 import com.mobile.utn.quecocino.RecipeGallery.Objects.Recipe;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import butterknife.ButterKnife;
@@ -49,6 +57,7 @@ public class RecipeGallery extends AppCompatActivity {
     private StorageReference mStorage;
     private ProgressDialog progressDialog;
     private static final int GALLERY_INTENT = 1;
+    private static final int CAMERA_INTENT = 0;
     private FirebaseDatabase database;
 
     @Override
@@ -105,44 +114,100 @@ public class RecipeGallery extends AppCompatActivity {
         progressDialog = new ProgressDialog(this);
         mStorage = FirebaseStorage.getInstance().getReference();
 
-        uploadButton.setOnClickListener(new View.OnClickListener() {
+        uploadButton.setOnClickListener(new View.OnClickListener(){
+
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                startActivityForResult(intent, GALLERY_INTENT);
+                final CharSequence[] options = {"Tomar foto", "Elegir de galería"};
+                final AlertDialog.Builder builder =  new AlertDialog.Builder(RecipeGallery.this);
+                builder.setItems(options, new DialogInterface.OnClickListener(){
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int option) {
+                        if(option == 0){
+                            openCamera();
+                        }else{
+                            openGallery();
+                        }
+                    }
+                });
+                builder.show();
             }
         });
 
+    }
+
+    private void openCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_INTENT);
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALLERY_INTENT);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == GALLERY_INTENT && resultCode == RESULT_OK){
-            String strProgress = getResources().getString(R.string.progress_uploadImage);
-            progressDialog.setMessage(strProgress);
-            progressDialog.setCancelable(false);
-            progressDialog.show();
+        if(resultCode == RESULT_OK){
 
-            Uri uri = data.getData();
-            StorageReference filePath = mStorage.child("recipes").child(uri.getLastPathSegment());
+            if(requestCode == GALLERY_INTENT){
 
-            filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                startProgressDialog();
 
-                    @SuppressWarnings("VisibleForTests")Uri uri = taskSnapshot.getMetadata().getDownloadUrl();
-
-                    Recipe recipe = new Recipe("Nueva Receta", "Un Autor", 12, uri.toString());
-                    database.getReference(RECIPE_REFERENCE).push().setValue(recipe);
-
-                    progressDialog.dismiss();
-                    Toast.makeText(RecipeGallery.this, R.string.toast_uploadSuccess, Toast.LENGTH_SHORT).show();
+                Uri uri = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                    pushImage(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            });
+
+            }else if(requestCode == CAMERA_INTENT){
+
+                startProgressDialog();
+                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                pushImage(bitmap);
+
+            }
 
         }
     }
+
+    private void pushImage(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream);
+
+        String nameFile = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        StorageReference filePath = mStorage.child("recipes").child(nameFile);
+        filePath.putBytes(stream.toByteArray()).addOnSuccessListener(getOnSuccessListener());
+    }
+
+
+    private OnSuccessListener<UploadTask.TaskSnapshot> getOnSuccessListener() {
+        return new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                @SuppressWarnings("VisibleForTests")Uri uri = taskSnapshot.getMetadata().getDownloadUrl();
+
+                Recipe recipe = new Recipe("Nueva Receta", "Un Autor", 12, uri.toString());
+                database.getReference(RECIPE_REFERENCE).push().setValue(recipe);
+
+                progressDialog.dismiss();
+                Toast.makeText(RecipeGallery.this, R.string.toast_uploadSuccess, Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
+
+    private void startProgressDialog() {
+        String strProgress = getResources().getString(R.string.progress_uploadImage);
+        progressDialog.setMessage(strProgress);
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+    }
+
 }
