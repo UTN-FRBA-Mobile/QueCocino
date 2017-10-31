fe06a40e9e20ec0e25e37ab83f1c4efedb646e57
ramiroyep <ramiro_yep@hotmail.com>
2017-10-30 21:20:57 -0300

    Adding GPSFilter

---
+++ QueCocino/app/src/main/java/com/mobile/utn/quecocino/recipes/filter/GPSFilter.java
@@ -0,0 +1,17 @@
+package com.mobile.utn.quecocino.recipes.filter;
+
+import com.mobile.utn.quecocino.model.Recipe;
+
+import java.util.List;
+
+/**
+ * Created by Rama on 30/10/2017.
+ */
+
+public class GPSFilter extends Filter {
+    @Override
+    public List<Recipe> meetCriteria(List<Recipe> recipes) {
+
+        return null;
+    }
+}
